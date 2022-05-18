6ed17b17aa10e574ed0f468a94681c01e902b107
NasserBiMBo <105250194+NasserBiMBo@users.noreply.github.com>
2022-05-18 15:32:00 +0200

    Add files via upload

---
+++ DisplayTime.c
@@ -0,0 +1,31 @@
+#include "lcd.h"
+void displayTime(int m,int s)
+{
+  LCD_command(0x01);
+ 
+  if(m>9){
+	
+	LCD_setcursorRowCol(0, 5);
+  LCD_printInt(m);
+	}
+	else{
+				LCD_setcursorRowCol(0, 5);
+				LCD_printInt(0);
+				LCD_setcursorRowCol(0, 6);
+				LCD_printInt(m);
+	}
+  LCD_setcursorRowCol(0, 7);
+  LCD_data(':');
+  if (s>9){LCD_setcursorRowCol(0, 8);
+  LCD_printInt(s);
+	}
+	else{
+		{LCD_setcursorRowCol(0, 8);
+			
+			LCD_printInt(0);
+			LCD_setcursorRowCol(0, 9);
+			LCD_printInt(s);
+	}
+		
+}
+}
