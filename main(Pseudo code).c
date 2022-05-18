371437ead33d6685d16dec790414b0bd60b88f76
antonsamoel <antonsamoel14@gmail.com>
2022-05-18 14:33:12 +0200

    Some Modification in functionality

--- main(Pseudo code).c
+++ main(Pseudo code).c
@@ -13,103 +13,6 @@
 #define finished 5
 
 
-void SWS_Init(void)
-{
-	SYSCTL_RCGCGPIO_R |=0x20;
-	while((SYSCTL_PRGPIO_R &0x20)==0);
-	
-	GPIO_PORTF_LOCK_R = 0x4c4f434b;
-	GPIO_PORTF_CR_R |=0x11;
-	GPIO_PORTF_PCTL_R &=~0x000F000F;
-	GPIO_PORTF_AMSEL_R&=~0x11;
-	GPIO_PORTF_AFSEL_R &=~0x11;
-	GPIO_PORTF_PUR_R |=0x11;
-	GPIO_PORTF_DEN_R |=0x11;
-	GPIO_PORTF_DIR_R &=~0x11;
-	
-}
-void RGB_Init_on(void)
-{
-	SYSCTL_RCGCGPIO_R |=0x20;
-	while((SYSCTL_PRGPIO_R &0x20)==0);
-	
-	GPIO_PORTF_LOCK_R = 0x4c4f434b;
-	GPIO_PORTF_CR_R |=0xE;
-	GPIO_PORTF_PCTL_R &=~0x0000FFF0;
-	GPIO_PORTF_AMSEL_R&=~0xE;
-	GPIO_PORTF_AFSEL_R &=~0xE;
-	GPIO_PORTF_DEN_R |=0xE;
-	GPIO_PORTF_DIR_R |=0xE;
-	GPIO_PORTF_DATA_R |=0xE;
-}
-// intialize sw3 in PD0
-void SW3_Init(void)
-{
-	SYSCTL_RCGCGPIO_R |=0x8;
-	while((SYSCTL_PRGPIO_R &0x8)==0);
-	
-	//GPIO_PORTF_LOCK_R = 0x4c4f434b;
-	GPIO_PORTF_CR_R |=0x11;
-	GPIO_PORTF_PCTL_R &=~0x0000000F;
-	GPIO_PORTF_AMSEL_R&=~0x01;
-	GPIO_PORTF_AFSEL_R &=~0x01;
-	GPIO_PORTF_PUR_R |=0x01;
-	GPIO_PORTF_DEN_R |=0x01;
-	GPIO_PORTF_DIR_R &=~0x01;
-}
-
-unsigned char SW1_Input(void)
-{
-	return (GPIO_PORTF_DATA_R&0x10);
-}
-	
-
-
-unsigned char SW3_Input(void)
-{
-	return (GPIO_PORTD_DATA_R&0x01);
-}
-
-unsigned char SW2_Input(void)
-{
-	return (GPIO_PORTF_DATA_R&0x1);
-}
-
-void RGB_Off()
-{
-	GPIO_PORTF_DATA_R&=~0xE;
-	
-}
-
-
-	bool isClosed()
-	{
-		if(SW3_Input()==0x1) // if Prssed => open
-		return true;
-		
-		else return false;
-	}
-	// Function to start
-	
-  void blink_3_times()
-	{
-		int num;
-		for(num=0;num<3;num++)
-	{
-		GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ 0x0E;
-		delay(10);
-		GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ 0x0E;
-		delay(100);
-	}
-	
-}
-	void blink()
-	{
-		GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ 0x0E;
-		delay(10);
-		GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ 0x0E;
-		delay(100);
-	}
 
 
 int main(void)
@@ -128,7 +31,7 @@ int main(void)
 			case idle:
 				// Function that take the input from the keypad
 			  // Wrong Choice => do while
-				// input == A => print "“Popcorn”"
+				// input == A => print "ï¿½Popcornï¿½"
 				// if input = b,c,d => state =  user_input
 			if(isClosed())
 			{
