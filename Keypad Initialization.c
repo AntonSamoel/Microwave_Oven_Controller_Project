3269e9446eff464a90e44cad4cf5d5f0c427c93f
NasserBiMBo <105250194+NasserBiMBo@users.noreply.github.com>
2022-05-18 15:32:34 +0200

    Add files via upload

---
+++ Keypad Initialization.c
@@ -0,0 +1,57 @@
+#ifndef __KEYPAD_H__
+#define __KEYPAD_H__
+#include "tm4c123gh6pm.h"
+#define  padRows  4
+#define  padCols  4
+
+void keypad_Init       (void);
+char keypad_getkey     (void);
+
+#endif
+
+// port c          C4,C5,C6,C7   FOR COLUMNS
+// port E          E1,E2,E3,E4   FOR RAWS
+
+
+unsigned int ncols = 0;
+unsigned int nrows = 0;                                                                    
+unsigned const char symbol[padRows][padCols] = {{ '1', '2',  '3', 'A'},      
+                                                { '4', '5',  '6', 'B'},      
+                                                { '7', '8',  '9', 'C'},      
+                                                { '*', '0',  '#', 'D'}};
+void systick_10MicroSec( double delay);
+																								
+																								
+																								
+																								
+void keypad_Init(void)
+{
+  SYSCTL_RCGCGPIO_R |= 0x14;            //enable clc for port C & E  
+  while ((SYSCTL_RCGCGPIO_R&0x14)==0);  //wait for clock to be enabled
+  GPIO_PORTC_CR_R  |= 0xF0;             //allow changes to all the bits in port C
+  GPIO_PORTE_CR_R  |= 0x1E;             //allow changes to all the bits in port E
+  GPIO_PORTC_DIR_R |= 0xF0;             //set directions cols are o/ps
+  GPIO_PORTE_PDR_R |= 0x1E;             //pull down resistor on Raws
+  GPIO_PORTC_DEN_R |= 0xF0;             //digital enable pins in port C
+  GPIO_PORTE_DEN_R |= 0x1E;             //digital enable pins in port E
+}
+
+char keypad_getkey(void)
+{
+  while(1)
+  {
+		int i;
+		int j;
+    for(i = 0; i < 4; i++)                        //columns traverse
+    {
+      GPIO_PORTC_DATA_R = (1u << (i+4));
+      systick_10MicroSec(0.2);
+			
+      for(j = 0; j < 4; j++)                     //raws traverse
+      {
+        if((GPIO_PORTE_DATA_R & 0x1E) & (1U << (j+1)))
+          return symbol[j][i];
+      }
+    }
+  }
+}
