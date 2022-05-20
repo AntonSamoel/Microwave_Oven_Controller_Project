f909edcc5be5b5907d89fa8de08a05332ab5238a
AntonSamoel <105069841+AntonSamoel@users.noreply.github.com>
2022-05-20 20:30:44 +0200

    Cooking State

---
+++ case_cooking.c
@@ -0,0 +1,30 @@
+case cooking:
+			 RGB_Init_on();
+				f=false;
+			 while(m >= 0)
+			 {
+				 while(s >= 0)
+				 {
+					  LCD_clear()
+						displayTime(m ,s);
+
+						genericDelay(50);              //200
+						s--;         
+						if(m==0 && s==0){
+							state = finished;
+							genericDelay(1);
+						}
+						if(SW1_Input() != 0x10 || !isClosed()){
+							genericDelay(1);
+							state = paused;
+							f=true;
+							break;
+						}
+						
+				}
+				 if(f){
+				 break;}
+				m--;
+				s = 59;      
+		}
+			break;
