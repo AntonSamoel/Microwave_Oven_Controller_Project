3c31530f2b24e18752211f0646410fc0d22f8ec4
Basha111 <105315970+Basha111@users.noreply.github.com>
2022-05-23 19:45:30 +0200

    Update Blinking_Functions.c

--- Blinking_Functions.c
+++ Blinking_Functions.c
@@ -5,17 +5,17 @@
 		for(num=0;num<3;num++)
 	{
 		GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ 0x0E;
-		genericDelay(10);
-		GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ 0x0E;
 		genericDelay(1000);
+		GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ 0x0E;
+		genericDelay(100);
 	}
 	
 }
 	void blink()
 	{
 		GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ 0x0E;
-		genericDelay(10);
+		genericDelay(250);
 		GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R ^ 0x0E;
-		genericDelay(10);
+		genericDelay(250);
 	}
 		
